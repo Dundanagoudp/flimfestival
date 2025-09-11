aabe2b2cd9d42922f16aeed9bbc9a6994e3be8a2
chetanv18~ <chetan@gully2global.com>
2025-09-11 16:10:49 +0530

    public layout updated

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/user/Home/hero/Herosection";
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 export default function Page() {
   return (
